79c70f507cc1f799f830c340a9f6a23702bb0ac1
Chris Chapman <chapmanchri@gmail.com>
2015-11-20 18:40:22 -0500

    add form in new.erb

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -13,4 +13,38 @@ class PinsController < ApplicationController
     render :show
   end
 
+  def new
+    @pin = Pin.new
+  end
+
+  def create
+    # puts "in def create"
+    @pin = Pin.create(pin_params)
+    # puts "the title should be: #{@pin.title}"
+    # puts "in create 1"
+    # @pin.save
+    # puts "in create SAVE"
+    # # redirect_to pins_path
+    # redirect_to pin_by_name_path(slug: @pin.slug)
+
+    respond_to do |format|
+      if @pin.save
+        puts "in save"
+        format.html { redirect_to @pin, notice: 'Event was successfully created.' }
+        format.json { render :show, status: :created, location: @pin }
+      else
+        puts "in not save"
+        format.html { render :new }
+        format.json { render json: @pin.errors, status: :unprocessable_entity }
+      end
+    end
+
+  end
+
+  private
+
+  def pin_params
+    params.require(:pin).permit(:title, :url, :slug, :text, :category_id)
+  end
+
 end
